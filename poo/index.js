67ac22f1461fade89c5b76b18dd00f3e602e3007
Paoko <felipe.ulloa@ug.uchile.cl>
2024-11-24 23:00:07 -0300

    Agregué un método votar para aumentar en 1 a la opción escogida, en caso de fallar tira error

--- poo/index.js
+++ poo/index.js
@@ -4,6 +4,13 @@ class Question {
     this.opciones = opciones;
     this.votos = new Array(opciones.length).fill(0); // con esto comienzo el contador de votos
     }
-}
 
+    votar(opcion) {
+        if (opcion >= 0 && opcion < this.opciones.length) {
+            this.votos[opcion]++;
+        } else {
+            console.log("No puedes ingresar esta opción");
+        }
+    }
+}
 
