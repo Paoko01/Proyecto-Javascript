e33ca7387fb601ad1f4ae87e41f90c81b2c9726b
Paoko <felipe.ulloa@ug.uchile.cl>
2024-11-24 23:24:10 -0300

    agregué método para mostrar los resultados en tiempo real, contiene un console.log para mostrar la pregunta, un forEach para recorrer el array con sus respectivos parámetros y una arrow function para ejecutar el código dentro de, un console.log que me sirve para mostrar la opción y su correspondiente cantidad de votaciones

--- poo/index.js
+++ poo/index.js
@@ -7,10 +7,18 @@ class Question {
 
     votar(opcion) {
         if (opcion >= 0 && opcion < this.opciones.length) {
-            this.votos[opcion]++;
+            this.votos[opcion]++;   //esto aumenta en 1 el contador para la opción escogida
         } else {
             console.log("No puedes ingresar esta opción");
         }
     }
-}
+
+    mostrarResultado() {
+        console.log(this.texto);    //esto muestra la pregunta
+        this.opciones.forEach((opcion, index) => {
+            console.log(`${opcion}: ${this.votos[index]} votos`)});  //con esto muestro la posición y la cantidad de votos asociada a cada elemento del array
+    }
+} //aquí termina la class Question
+
+
 
